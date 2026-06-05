52957484cd237f56f64d22c0cc114293313f4177
Krish <krishj19731@gmail.com>
2026-06-05 17:36:20 +0530

    Added signup logic

---
+++ src/services/signup.service.js
@@ -0,0 +1,20 @@
+import { userRepository } from '../repositories/user.repository.js'
+import { ValidationError } from '../utils/errors/validationerror.js'
+
+export async function signUpService(data) {
+  try {
+    const newUser = await userRepository.create(data)
+    return newUser
+  } catch (error) {
+    console.log('Signup service error: ', error)
+    if (error.name === 'ValidationError') {
+      throw new ValidationError({ error: error.errors }, error.message)
+    }
+    if (error.name === 'MongoServerError' && error.code === 11000) {
+      throw new ValidationError(
+        { error: 'Email or username already exists' },
+        'Duplicate key error'
+      )
+    }
+  }
+}
