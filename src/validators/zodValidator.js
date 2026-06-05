d5d1ffd8056a4a66e833fff10e63f5c024dcd5f0
Krish <krishj19731@gmail.com>
2026-06-05 20:22:20 +0530

    Fixed Validate Request Body

--- src/validators/zodValidator.js
+++ src/validators/zodValidator.js
@@ -1,15 +1,22 @@
-import { StatusCodes } from "http-status-codes"
+import { StatusCodes } from 'http-status-codes'
 
-export function validateRequestBody(schema){
-    return  async function (req, res, next) {
-        try{
-            const data =await schema.parseAsync(req.body)
-            next()
-        }catch(error){
-            res.status(StatusCodes.BAD_REQUEST).json({
-                success: false,
-                message : 'Validation error'
-            })
-        }
+export function validateRequestBody(schema) {
+  return async function (req, res, next) {
+    try {
+      const data = await schema.parseAsync(req.body)
+      next()
+    } catch (error) {
+      console.log('(Zod Validator)Validation error details: ', error.issues)
+      //   console.log('Type ', typeof error.message)
+      let explanation = []
+      for (const issue of error.issues) {
+        explanation.push(`${issue.path} : ${issue.message}`)
+      }
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: 'Validation error',
+        explanation: explanation
+      })
     }
+  }
 }
